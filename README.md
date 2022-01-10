e03842c70f6f3bc863944d27133b25fd36babbb1
Luis Bernal <lbernal@dynatronsoftware.com>
2022-01-10 00:09:05 -0600

    Added a lot, need to work on commiting more!

---
+++ README.md
@@ -0,0 +1,6 @@
+Thank you for visiting my small front-end application.
+
+This is a very simple app that makes a fetch request to NASA's API and then displays the info in cards I built.
+I have not worked with front-end in a while since I've started my support engineer role, so this was good refresher. I also implemented a lot of things I had not used before, an example, CSS grids. This simple app was built using vanilla JS, CSS and HTML. Font-awesome was used for the heart shaped icon and the loading wheel. If you downloaded my code from GitHub, you can run the app by simply opening the index.html on your google browser. If you haven't checked out my GitHub, here the link to this project, https://github.com/LRB8010/Spaceify. 
+
+Once again, thank you!
--- index.js
+++ index.js
@@ -1,42 +1,61 @@
 
+    let divLoader = document.querySelector(".loader")
+    console.log(divLoader)
+//'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=98Jn5EfwNiD6OHjI3IbxZYofdV1mTsqzYf4BySV6'
+createLoader()
 
-'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=98Jn5EfwNiD6OHjI3IbxZYofdV1mTsqzYf4BySV6'
 fetch("https://api.nasa.gov/planetary/apod?start_date=2021-12-02&end_date=2022-01-01&api_key=98Jn5EfwNiD6OHjI3IbxZYofdV1mTsqzYf4BySV6")
 .then(response =>{
     return response.json();
 })
-.then(pic =>{
-    console.log(pic)
-    pic.forEach(e => {   
-        if(e.media_type == "image"){
+.then(data =>{
+    hideLoader()
+    console.log(data)
+    data.forEach(elem => {   
+        if(elem.media_type == "image"){
         let grid = document.querySelector(".grid");
         let card = document.createElement("div");
         let gridItem = document.createElement("div");
         let img = document.createElement("img");
         let title = document.createElement("h3");
         let desc = document.createElement("p");
-        let button = document.createElement("button")
+        let descbutton = document.createElement("button")
         let date = document.createElement("p")
-        desc.innerText = e.explanation
-        title.innerText = e.title;
-        date.innerText = `Date of Capture: ${e.date}`
-        img.src = e.hdurl;
+        let likebtn = document.createElement("button")
+        let likes = document.createElement("p")
+        let icon =  document.createElement("i")
+        let br = document.createElement("p")
+        desc.innerText = elem.explanation
+        title.innerText = elem.title;
+        date.innerText = `Date: ${elem.date}`
+        descbutton.innerText = "Description"
+        br.innerText = "ㅤ"
+        likes.innerText = `${Math.floor(Math.random() * 10000)}`
+        img.src = elem.hdurl;
         gridItem.className = "gridItem";
         desc.className = "para"
-        button.addEventListener("click",showDescription)
+        likes.className = "like"
+        card.className = "card";
+        likebtn.className = "far fa-heart fa-xs"
+        date.className = "date"
+        descbutton.className = "descbttn"
+        likebtn.id = "likebtn"
+        likebtn.dataset.clicked = "false"
+        descbutton.addEventListener("click",showDescription)
+        likebtn.addEventListener("click",function(event){likeIncrease(event)})
+        img.addEventListener("mouseover",function(e){createTags(e)})
+        likebtn.append(icon)
         grid.append(gridItem)
         gridItem.append(card)
-        card.className = "card";
-        card.append(title,img,date,button,desc);
+        card.append(title,img,date,likebtn,likes,br,descbutton,desc);
         }
-    });
+    })
 })
 
 function showDescription(){
     let active = false
     this.classList.toggle("active");
     var content = this.nextElementSibling
-    console.log(content.style)
     if(content.style.display === "block")
     {
         content.style.display = "none"
@@ -46,3 +65,41 @@ function showDescription(){
 }
 
 
+function likeIncrease(event){
+    let target = event.target
+    let numberLike = target.nextElementSibling
+    console.log(target.dataset.clicked)
+    if(target.dataset.clicked === "true" ){
+        console.log("Always")
+        target.dataset.clicked = "false"
+        target.className = "far fa-heart fa-xs"
+        numberLike.innerText = parseInt(numberLike.innerText) - 1   
+    }else{
+        target.dataset.clicked = "true"
+            console.log("First")
+        target.className = "fas fa-heart fa-xs likeAnimation"    
+    numberLike.innerText = parseInt(numberLike.innerText) + 1
+    }
+}
+
+
+function createTags(e){
+    console.log("Working")
+    let p = document.createElement("p")
+    p.innerText = "Hello"
+    e.target.append(p)
+}
+
+function createLoader(){
+    let divLoader = document.querySelector(".loader")
+    console.log(divLoader)
+    let loading = document.createElement("i")
+    loading.className = "fas fa-spinner fa-spin fa-5x loading"
+    divLoader.append(loading)
+}
+
+function hideLoader(){
+    console.log("Loaded")
+    let divLoader = document.querySelector(".fa-spinner")
+    divLoader.style.display = "none"
+}
